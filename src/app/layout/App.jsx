f04a090177caa26f31a50f94b972750e64888bee
james-t-rose <68847165+james-t-rose@users.noreply.github.com>
2021-07-21 11:38:31 +0100

    Update
    Quick Update
    - Nav Bar
    - UI Elements

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/event-dashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+
+export default function App() {
+  
+  return (
+    <>
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
+    </>
+  );
+}
+
+
