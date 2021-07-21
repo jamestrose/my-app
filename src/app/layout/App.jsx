1872c58261464a1bda922f4bf89d411b488107e8
james-t-rose <james.t.rose@hotmail.com>
2021-07-21 12:42:39 +0100

    Buttons
    Form Buttons

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/event-dashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
 
 export default function App() {
-  
+  const [formOpen, setFormOpen] = useState(false);
   return (
     <>
-      <NavBar />
+      <NavBar setFormOpen={setFormOpen}/>
       <Container className='main'>
-        <EventDashboard />
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
       </Container>
     </>
   );
